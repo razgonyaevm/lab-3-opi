145108f00e772106b14d99a3835c956f1766c21d
razgonyaevm <razgonyaevm@gmail.com>
2026-05-10 11:48:24 +0500

    Main commit

---
+++ src/main/java/com/areaapp/Main.java
@@ -0,0 +1,7 @@
+package com.areaapp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ant build success! Lab #3");
+    }
+}
---
+++ src/test/java/com/areaapp/AppTest.java
@@ -0,0 +1,16 @@
+package com.areaapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testSimple() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testHello() {
+        assertEquals(4, 2 + 2);
+    }
+}
